86eb2f9d7aba26acd069f8de38f9dd9516250af0
Danny Schreiber <danny@ravenartmedia.com>
2014-08-22 14:09:31 -0700

    refactored angular sites controller and hooked up webApi rest layer for gsr, sites, and gsrReport

---
+++ api/psp.core/domain/IClient.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace psp.core.domain
+{
+    public interface IClient
+    {
+        string address { get; set; }
+        bool addressverified { get; set; }
+        DateTime birthdate { get; set; }
+        bool birthdaycouponsent { get; set; }
+        string city { get; set; }
+        DateTime dateregistered { get; set; }
+        string email { get; set; }
+        string firstname { get; set; }
+        MongoDB.Bson.ObjectId Id { get; set; }
+        bool isactive { get; set; }
+        string lastname { get; set; }
+        string phone { get; set; }
+        bool receivenotifications { get; set; }
+        string sid { get; set; }
+        string state { get; set; }
+        string zip { get; set; }
+    }
+}
